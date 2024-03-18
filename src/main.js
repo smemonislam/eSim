ecd58857276ff4249947ba6cd09db2d672e2bcbd
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2024-03-19 03:45:38 +0600

    remove

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import "./assets/css/all.min.css";
 import "./assets/css/bootstrap.min.css";
-import "./assets/js/jquery.min.js";
-import "./assets/js/bootstrap.bundle.min.js";
-import "./assets/js/admin.min.js";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
